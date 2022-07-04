865d29fb5c6e49f9b82e663c5b2b13f89eb7a723
Abhinav pandey <pandeysandeep1190@gmail.com>
2022-07-04 03:34:47 -0700

    mad

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -7,7 +7,7 @@ export default async function middleware(req){
     const url = req.url
     
     
-    if(url.includes('https://ecommerce123.vercel.app/products') || url.includes('https://ecommerce123.vercel.app/reviews')){
+    if(url.includes('https://ecommerce123.vercel.app/products')){
         
         if(token == undefined){
             return NextResponse.redirect('https://ecommerce1234.vercel.app/login')
